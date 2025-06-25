4849add2ba167b73c065cb31198f9cb190bc7b08
Dika Afty <dikaafty@gmail.com>
2025-06-25 22:01:11 +0700

    Import NotFound component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./components/Homepage";
+import NotFound from "./components/NotFound";
 
 function App() {
   return (
