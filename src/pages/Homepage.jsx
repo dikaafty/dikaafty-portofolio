072ef9a481159d2f340c779b9e45424198fe47e7
Dika Afty <dikaafty@gmail.com>
2025-06-26 19:48:10 +0700

    Import Navbar component

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,5 +1,6 @@
 import ThemeToggle from "../components/ThemeToggle";
 import Background from "@/components/Background";
+import Navbar from "@/components/Navbar";
 
 const Homepage = () => {
   return (
