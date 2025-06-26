8ce67f2d419c9cf1355cdfa59c7762808dc8ad55
Dika Afty <dikaafty@gmail.com>
2025-06-26 13:30:19 +0700

    Declare useEffect hook and create its logic in ThemeToggle component

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -3,6 +3,18 @@ import { useState, useEffect } from "react";
 const ThemeToggle = () => {
   const [ isDarkMode, setIsDarkMode ] = useState(false);
 
+  useEffect(() => {
+    const storedTheme = localStorage.getItem("theme");
+
+    if(storedTheme === "dark") {
+      setIsDarkMode(true);
+      document.documentElement.classList.add("dark");
+    } else {
+      localStorage.setItem("theme", "light");
+      setIsDarkMode(false);
+    }
+  }, []);
+
   const toggleTheme = () => {
     if(isDarkMode) {
       localStorage.setItem("theme", "light");
