91b9c1b361332c1e964c67eb4e972a69319e2a33
Dika Afty <dikaafty@gmail.com>
2025-06-26 19:51:50 +0700

    Create navItems array and its items

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,11 @@
+const navItems = [
+  {name: "Home", href: "#hero"},
+  {name: "About", href: "#about"},
+  {name: "Skills", href: "#skills"},
+  {name: "Projects", href: "#projects"},
+  {name: "Contact", href: "#contact"},
+];
+
 const Navbar = () => {
   return (
     <nav>
