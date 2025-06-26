266d2208d9feaf3701db00da72a8ed31acf1d461
Dika Afty <dikaafty@gmail.com>
2025-06-26 14:45:01 +0700

    Render each meteor in Background component

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -67,6 +67,23 @@ const Background = () => {
           />
         ))
       }
+
+      {
+        meteors.map((meteor) => (
+          <div 
+            key={meteor.id} 
+            className="meteor animate-meteor" 
+            style={{
+              width: `${meteor.size * 50}px`,
+              height: `${meteor.size * 2}px`,
+              left: `${meteor.x}%`,
+              top: `${meteor.y}%`,
+              animationDelay: `${meteor.delay}`,
+              animationDuration: `${meteor.animationDuration}s`
+            }}
+          />
+        ))
+      }
     </div>
   )
 }
