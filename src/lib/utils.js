b9dc3bec3f4874e908c2c53e9d7aa57d05ad1cc1
Dika Afty <dikaafty@gmail.com>
2025-07-02 01:38:48 +0700

    Create runObserver function, create its logic and export it

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -4,3 +4,24 @@ import { twMerge } from "tailwind-merge";
 export const cn = (...inputs) => {
   return twMerge(clsx(inputs));
 }
+
+export const runObserver = (...refs) => {
+  const options = {
+    root: null,
+    threshold: 0.5
+  };
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      const animationClass = entry.target.dataset.animate;
+
+      entry.target.classList.toggle(animationClass, entry.isIntersecting);
+    });
+  }, options);
+
+  refs.forEach((ref) => {
+    if(ref.current) observer.observe(ref.current);
+  });
+
+  return () => observer.disconnect();
+}
