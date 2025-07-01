f9ceb7733213f54be478c2714cd31c1e42eba6f8
Dika Afty <dikaafty@gmail.com>
2025-07-01 18:52:20 +0700

    Change id of section jsx in HeroSection component

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -3,7 +3,7 @@ import CosmicButton from "./CosmicButton";
 const HeroSection = () => {
   return (
     <section 
-      id="#hero"
+      id="hero"
       className="relative min-h-screen flex flex-col justify-center items-center px-4"
     >
       <div 
