821dafba14745df312cd0f831596199437a01129
Dika Afty <dikaafty@gmail.com>
2025-07-02 13:46:15 +0700

    Create skills array and its items

--- src/components/SkillsSection.jsx
+++ src/components/SkillsSection.jsx
@@ -1,3 +1,20 @@
+const skills = [
+  // Frontend
+  {name: "HTML", level: 85, category: "Frontend"},
+  {name: "CSS", level: 85, category: "Frontend"},
+  {name: "Tailwind CSS", level: 85, category: "Frontend"},
+  {name: "JavaScript", level: 80, category: "Frontend"},
+  {name: "TypeScript", level: 50, category: "Frontend"},
+  {name: "Webpack", level: 50, category: "Frontend"},
+  {name: "React", level: 80, category: "Frontend"},
+
+  // Tools
+  {name: "GIT", level: 80, category: "Frontend"},
+  {name: "GitHub", level: 80, category: "Frontend"},
+  {name: "Figma", level: 50, category: "Frontend"},
+  {name: "VS Code", level: 90, category: "Frontend"},
+];
+
 const SkillsSection = () => {
   return (
     <section>
