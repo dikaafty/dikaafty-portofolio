7d564c9df859d89f570d8777f720666df77c614c
Dika Afty <dikaafty@gmail.com>
2025-07-02 13:38:25 +0700

    Import SkillsSection component

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -3,6 +3,7 @@ import Background from "@/components/Background";
 import Navbar from "@/components/Navbar";
 import HeroSection from "@/components/HeroSection";
 import AboutSection from "../components/AboutSection";
+import SkillsSection from "../components/SkillsSection";
 
 const Homepage = () => {
   return (
