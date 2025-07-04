62c73058a7b304df16f0d05affe7d004451bbb41
Dika Afty <dikaafty@gmail.com>
2025-07-04 20:19:12 +0700

    Change title value of all projects object in projects array

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -1,7 +1,7 @@
 const projects = [
   {
     id: 1,
-    title: "EAT. - Restaurant Web",
+    title: "EAT.",
     description: "A simple, clean, and minimalistic restaurant website built using HTML, CSS, JavaScript and Webpack. This project is part of The Odin Project.",
     image: "/images/project-one.png",
     demoUrl: "https://dikaafty.github.io/odin-restaurant-page/",
@@ -9,7 +9,7 @@ const projects = [
   },
   {
     id: 2,
-    title: "VideoBelajar - Learning Web",
+    title: "VideoBelajar",
     description: "A simple and responsive web-based video learning platform designed to help users access educational content anytime, anywhere.",
     image: "/images/project-two.png",
     demoUrl: "https://videobelajar-api-redux.netlify.app/",
@@ -17,7 +17,7 @@ const projects = [
   },
   {
     id: 3,
-    title: "Uno.do - Todo List Web",
+    title: "Uno.do",
     description: "A clean and interactive Todo List application to manage your daily tasks efficiently. This project offers features like task categorization, editing, and completion tracking.",
     image: "/images/project-three.png",
     demoUrl: "https://dikaafty.github.io/todo-list/",
