a9de618e4aee99abe7d3e93720377b20696b078e
Dika Afty <dikaafty@gmail.com>
2025-07-04 18:23:51 +0700

    Import ProjectsSection component

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -4,6 +4,7 @@ import Navbar from "@/components/Navbar";
 import HeroSection from "@/components/HeroSection";
 import AboutSection from "../components/AboutSection";
 import SkillsSection from "../components/SkillsSection";
+import ProjectsSection from "../components/ProjectsSection";
 
 const Homepage = () => {
   return (
