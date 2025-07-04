531b4070cfc4dfe4c00dd4e78423a276858f6262
Dika Afty <dikaafty@gmail.com>
2025-07-04 19:36:50 +0700

    Create projects array and its values

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -1,3 +1,30 @@
+const projects = [
+  {
+    id: 1,
+    title: "EAT. - Restaurant Web",
+    description: "A simple, clean, and minimalistic restaurant website built using HTML, CSS, JavaScript and Webpack. This project is part of The Odin Project.",
+    image: "/images/project-one.png",
+    demoUrl: "https://dikaafty.github.io/odin-restaurant-page/",
+    githubUrl: "https://github.com/dikaafty/odin-restaurant-page"
+  },
+  {
+    id: 2,
+    title: "VideoBelajar - Learning Website",
+    description: "A simple and responsive web-based video learning platform designed to help users access educational content anytime, anywhere.",
+    image: "/images/project-two.png",
+    demoUrl: "https://videobelajar-api-redux.netlify.app/",
+    githubUrl: "https://github.com/dikaafty/videobelajar-api-redux"
+  },
+  {
+    id: 3,
+    title: "Uno.do - Todo List Web",
+    description: "A clean and interactive Todo List website to manage your daily tasks efficiently.",
+    image: "/images/project-three.png",
+    demoUrl: "https://dikaafty.github.io/todo-list/",
+    githubUrl: "https://github.com/dikaafty/todo-list"
+  }
+];
+
 const ProjectsSection = () => {
   return (
     <section>
