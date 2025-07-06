8213f45e66e475adac94ee603fb496b1a2b7ff8a
Dika Afty <dikaafty@gmail.com>
2025-07-06 12:28:18 +0700

    Change isDarkMode state initial value in ThemeToggle component

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -3,7 +3,7 @@ import { Sun, Moon } from "lucide-react";
 import { cn } from "@/lib/utils";
 
 const ThemeToggle = () => {
-  const [ isDarkMode, setIsDarkMode ] = useState(false);
+  const [ isDarkMode, setIsDarkMode ] = useState(true);
 
   useEffect(() => {
     const storedTheme = localStorage.getItem("theme");
