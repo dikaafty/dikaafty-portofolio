f2d0d6d2f4903394c06ad8e43f59a18478e28a5e
Dika Afty <dikaafty@gmail.com>
2025-07-07 01:40:51 +0700

    Create onSubmit function, its logic and export onSubmit function

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -31,3 +31,26 @@ export const runObserver = (...refs) => {
 
   return () => observer.disconnect();
 }
+
+export const onSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "634b5db6-87e8-4aa5-a920-94afac716dd9");
+
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+
+    const res = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: json
+    }).then((res) => res.json());
+
+    if (res.success) {
+      console.log("Success", res);
+    }
+  };
