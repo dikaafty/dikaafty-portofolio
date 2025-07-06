4b271f70e76adff4d39c972c410a7610c1f4bc89
Dika Afty <dikaafty@gmail.com>
2025-07-07 00:19:53 +0700

    Render Toaster component in App component

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { Toaster } from "./components/ui/toaster";
 function App() {
   return (
     <>
+      <Toaster />
       <BrowserRouter>
         <Routes>
           <Route index element={<Homepage />} />
