236e21648727d12a1d6fd2b3875c362151c788f2
Dika Afty <dikaafty@gmail.com>
2025-07-07 02:07:59 +0700

    Import Footer component

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -6,6 +6,7 @@ import AboutSection from "../components/AboutSection";
 import SkillsSection from "../components/SkillsSection";
 import ProjectsSection from "../components/ProjectsSection";
 import ContactSection from "../components/ContactSection";
+import Footer from "../components/Footer";
 
 const Homepage = () => {
   return (
