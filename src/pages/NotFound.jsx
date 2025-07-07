d195db8a50d69ec8813dbf1a148d092be38fa1a9
Dika Afty <dikaafty@gmail.com>
2025-07-07 23:22:37 +0700

    Create not found content container and its items jsx in NotFound component

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -6,6 +6,30 @@ const NotFound = () => {
   return (
     <div className="bg-[#05080F]">
       <Background />
+
+      <div className="w-screen h-screen flex flex-col justify-center items-center gap-9">
+        <div className="px-5 flex flex-col">
+          <p className="text-xs sm:text-sm text-background font-jost font-medium select-none tracking-widest">
+            PAGE NOT FOUND
+          </p>
+
+          <p className="text-7xl sm:text-8xl text-background font-jost font-bold select-none">
+            404
+          </p>
+
+          <p className="text-sm sm:text-base text-background font-jost font-semibold select-none mt-4 tracking-wider">
+            I'M SORRY, BUT THE PAGE YOU REQUESTED WAS NOT FOUND
+          </p>
+        </div>
+
+        <a 
+          href="/"
+          className="cosmic-button flex items-center gap-2"
+        >
+          <CircleArrowLeft />
+          Go Back
+        </a>
+      </div>
     </div>
   )
 }
