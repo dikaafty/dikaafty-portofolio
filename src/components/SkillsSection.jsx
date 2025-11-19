0645b184f7a1b6464db59a9a6092a76df6acd4f8
Dika Afty <dikaafty@gmail.com>
2025-11-19 22:32:56 +0700

    Add handleOnMouseMove function and its logic in SkillsSection

--- src/components/SkillsSection.jsx
+++ src/components/SkillsSection.jsx
@@ -42,6 +42,17 @@ const SkillsSection = () => {
     (skill) => activeCategory === "all" || skill.category === activeCategory
   );
 
+  const handleOnMouseMove = (e) => {
+    const { currentTarget: target } = e;
+
+    const rect = target.getBoundingClientRect(),
+          x = e.clientX - rect.left,
+          y = e.clientY - rect.top;
+
+    target.style.setProperty("--mouse-x", `${x}px`);
+    target.style.setProperty("--mouse-y", `${y}px`);
+  };
+
   useEffect(() => {
     const cleanUp = runObserver(
       sectionTitleRef, dividerOneRef, dividerTwoRef, skillsRef, categoriesRef
