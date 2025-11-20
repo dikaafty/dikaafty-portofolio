060d8e1aafaa86e155ce67efe7b2bd46a1b9c6b9
Dika Afty <dikaafty@gmail.com>
2025-11-20 10:53:59 +0700

    Update logic of handleOnMouseMove function in SkillsSection

--- src/components/SkillsSection.jsx
+++ src/components/SkillsSection.jsx
@@ -43,14 +43,15 @@ const SkillsSection = () => {
   );
 
   const handleOnMouseMove = (e) => {
-    const { currentTarget: target } = e;
+    for (const skill of skillsRef.current) {
+      const target = skill.current;
+      const rect = target.getBoundingClientRect(),
+        x = e.clientX - rect.left,
+        y = e.clientY - rect.top;
 
-    const rect = target.getBoundingClientRect(),
-          x = e.clientX - rect.left,
-          y = e.clientY - rect.top;
-
-    target.style.setProperty("--mouse-x", `${x}px`);
-    target.style.setProperty("--mouse-y", `${y}px`);
+      target.style.setProperty("--mouse-x", `${x}px`);
+      target.style.setProperty("--mouse-y", `${y}px`);
+    }
   };
 
   useEffect(() => {
