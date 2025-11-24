3957c4c0e4e0b2a3fab37c82bbe6e5ac74db9bc7
Dika Afty <dikaafty@gmail.com>
2025-11-24 22:50:20 +0700

    Declare useEffect with logic to initialize lenis and configure lenis smooth scroll in SmoothScroll

--- src/components/SmoothScroll.jsx
+++ src/components/SmoothScroll.jsx
@@ -2,6 +2,17 @@ import { useEffect } from "react";
 import Lenis from "lenis";
 
 const SmoothScroll = () => {
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    const raf = (time) => {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    };
+
+    requestAnimationFrame(raf);
+  }, []);
+
   return null;
 };
 
